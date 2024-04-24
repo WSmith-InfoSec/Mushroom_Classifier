b7a8d266ffb9dde33c418c85845fef247a19d76d
W. Smith <109860969+WSmith-InfoSec@users.noreply.github.com>
2024-04-23 19:17:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ async function runExample() {
 
     let session = new onnx.InferenceSession();
 
-    await session.loadModel("");
+    await session.loadModel("DLnet_MushroomData.onnx");
     let outputMap = await session.run([tensorX]);
     let outputData = outputMap.get('output1');
 
